1d69f6dc4ec65bee8759d1c7372ed53381e6b346
guerfala <mohamedtaher.guerfala@esprit.tn>
2023-07-27 13:23:29 +0100

    form en cours

--- src/Controller/InterventionsController.php
+++ src/Controller/InterventionsController.php
@@ -4,15 +4,60 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Interventions;
+use App\Form\InterventionFormType;
+use App\Repository\InterventionsRepository;
+
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class InterventionsController extends AbstractController
 {
-    #[Route('/interventions', name: 'app_interventions')]
+    #[Route('/', name: 'app_interventions')]
     public function index(): Response
     {
         return $this->render('interventions/index.html.twig', [
             'controller_name' => 'InterventionsController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_interventions_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, InterventionsRepository $InterventionsRepository): Response
+    {
+        $interventions = new Interventions();
+        $form = $this->createForm(InterventionFormType::class, $interventions);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $file = $form->get('piece_jointe')->getData();
+
+            if ($file) {
+                $fileName = $file->getPieceJointe();
+    
+                try {
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    // Handle the exception
+                }
+                $interventions->setPieceJointe($fileName);
+            }
+
+            $InterventionsRepository->save($interventions, true);
+
+            return $this->redirectToRoute('app_interventions', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('interventions/new.html.twig', [
+            'interventions' => $interventions,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/InterventionFormType.php
+++ src/Form/InterventionFormType.php
@@ -7,6 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints\File as AssertFile;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
 class InterventionFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -20,8 +26,20 @@ class InterventionFormType extends AbstractType
             ->add('batiment')
             ->add('local')
             ->add('description')
-            ->add('piece_jointe')
-            ->add('deg_urgence')
+            ->add('piece_jointe',FileType::class, [
+                'label' => 'Importer une piece jointe',
+                'required' => false,
+                'mapped' => false,
+                ])
+                ->add('deg_urgence', ChoiceType::class, [
+                    'choices' => [
+                        'Immediat' => 'immediat',
+                        '3 jours' => '3jours',
+                        '15 jours' => '15jours',
+                        'Mois' => 'mois',
+                        'Immediat' => 'immediat',
+                    ],
+                ])
         ;
     }
 
